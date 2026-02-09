12dbd9d980152d5ed710b932a848bad34671afc2
Alexie <none>
2026-02-09 08:38:46 -0500

    Fixed mixed ssl

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+    
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
+        }
+    
         // Define gates to use Role checks in "can" middleware/menus
         \Illuminate\Support\Facades\Gate::define('Admin', function ($user) {
             return $user->hasRole('Admin');
